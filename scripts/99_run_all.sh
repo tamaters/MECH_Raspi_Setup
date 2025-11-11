da22c346a2a5ec4c33c920e05ecebb2a78cb87bf
tamaters <joschka.maters@hslu.ch>
2025-11-11 13:56:47 +0100

    Update 99_run_all.sh

--- scripts/99_run_all.sh
+++ scripts/99_run_all.sh
@@ -6,10 +6,7 @@ bash scripts/05_system_update.sh
 bash scripts/06_groups_users.sh
 bash scripts/10_create_global_venv.sh
 bash scripts/20_install_packages.sh
-bash scripts/25_motd_setup.sh
 bash scripts/30_shell_activation.sh
-bash scripts/35_vnc_config.sh
-bash scripts/40_install_oled_netinfo.sh
 bash scripts/45_mech_lab_reset_home_dir.sh
 
 echo "[OK] Komplettes Setup abgeschlossen"
